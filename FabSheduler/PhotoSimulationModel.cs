bdd2937c7d708364bac0b3732be01aba02ad7607
ChangRyeol <changryeol.recruit@gmail.com>
2025-02-23 09:58:28 +0900

    [Model] : 공장 시뮬레이션 시작, 끝 로그 작성

---
+++ FabSheduler/PhotoSimulationModel.cs
@@ -0,0 +1,18 @@
+﻿using SimulationEngine.SimulationInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabShedulerModel
+{
+    public class PhotoSimulationModel : ISimulationModel
+    {
+        public void OnBeginInitialize() => Console.WriteLine("[FabModel] OnBeginInitialize triggered.");
+        public void OnEndInitialize() => Console.WriteLine("[FabModel] OnEndInitialize triggered.");
+        public void OnStart() => Console.WriteLine("[FabModel] OnStart triggered.");
+        public void OnDayChanged(DateTime date) => Console.WriteLine($"[FabModel] OnDayChanged triggered for {date.ToShortDateString()}.");
+        public void OnDone() => Console.WriteLine("[FabModel] OnDone triggered.");
+    }
+}
---
+++ FabSheduler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataMart.SqlMapper;
+using DataMart.Input;
+using FabShedulerModel;
+using SimulationEngine.SimulationEntity;
+
+namespace FabSchedulerModel
+{
+internal class Program
+{
+        static void Main(string[] args)
+        {
+      
+            InputMart.Instance.LoadFromDatabase();
+            //var products = InputMart.Instance.GetList<PRODUCT>(InputTable.PRODUCT);
+
+            DateTime startDate = new DateTime(2025, 3, 1, 15, 0, 0);
+            DateTime endDate = new DateTime(2025, 3, 5, 15, 0, 0);
+
+            PhotoSimulationModel model = new PhotoSimulationModel();
+
+            SimFactory.InitializeInstance(model, startDate, endDate);
+            SimFactory factory = SimFactory.Instance;
+            factory.Initialize();
+            factory.StartSimulation();
+
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+
+        }
+    }
+}
