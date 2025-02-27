d3a0d858b55f0d9118950964c6d0a95e120e51dc
ChangRyeol <changryeol.recruit@gmail.com>
2025-02-27 21:55:34 +0900

    [Engine] 시뮬레이션 base 객체 추가

---
+++ SimulationEngine/BaseEntity/Lot.cs
@@ -0,0 +1,21 @@
+﻿using SimulationEngine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationEngine.BaseEntity
+{
+    public abstract class Lot
+    {
+        public string LotId { get; set; }
+        public string ProductId { get; set; }
+        public string ProcessId { get; set; }
+        public string StepId { get; set; }
+        public string EqpId { get; set; }
+        public int LotQty { get; set; }
+        public LotState State { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+}
---
+++ SimulationEngine/BaseEntity/Step.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationEngine.BaseEntity
+{
+    public abstract class Step
+    {
+        public string StepId { get; set; }
+        public int StepSeq { get; set; }
+        //public string StepName { get; set; }
+        //public Step GetDefaultNextStep(); 
+    }
+}
