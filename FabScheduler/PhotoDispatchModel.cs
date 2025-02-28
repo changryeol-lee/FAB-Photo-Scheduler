7e69381aacaa74eb160294504d22e97c95ec4507
ChangRyeol <changryeol.recruit@gmail.com>
2025-02-28 10:22:50 +0900

    [Model] Dispatch Model, Eqp Model 개발
    [Dispatch Model]
    - fifo rule에 맞춰 lot 선택하도록 개발.
    - dispatch log 개발
    [Eqp Model]
    - Eqp 초기화 로직 개발
    - Eqp Arrange에 따른 선택 가능 eqp 반환 로직 개발

---
+++ FabScheduler/PhotoDispatchModel.cs
@@ -0,0 +1,57 @@
+﻿using DataMart.Output;
+using DataMart.SqlMapper;
+using SimulationEngine.Common;
+using SimulationEngine.ProcessEntity;
+using SimulationEngine.SimulationEntity;
+using SimulationEngine.SimulationInterface;
+using SimulationEngine.SimulationLog;
+using SimulationEngine.SimulationObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FabSchedulerModel
+{
+    public class PhotoDispatchModel : ISimDispatchModel
+    {
+        public void FilterLot(SimEquipment equipment, List<SimLot> candidateLots, out List<SimLot> passedLots, out List<SimLot> excludedLots)
+        {
+            passedLots = new List<SimLot>();
+            excludedLots = new List<SimLot>();
+
+            foreach (var lot in candidateLots)
+            {
+                if (lot.GetLot().State == LotState.WAIT)
+                    passedLots.Add(lot);
+                else
+                    excludedLots.Add(lot);
+            }
+        }
+
+        public bool FilterEqp(SimEquipment equipment, DateTime currentTime)
+        {
+            return false; 
+        }
+
+        public SimLot SelectLot(SimEquipment equipment, List<SimLot> filteredList)
+        {
+            return filteredList.OrderBy(x => x.GetLot().ArrivalTime).First();
+        }
+
+        public void WriteDispatchLog(DispatchLog log)
+        {
+            DISPATCH_LOG dl = new DISPATCH_LOG();
+            dl.EQP_ID = log.EqpId; 
+            dl.STEP_ID = log.StepId;
+            dl.DISPATCHING_TIME = log.DispatchingTime; 
+            dl.LOT_ID = log.SelectedLot.LotId;
+            dl.CANDIDATE_LOTS = string.Join(";", log.CandidateLots.Select(x=>x.LotId));
+            dl.PASSED_LOTS = string.Join(";", log.PassedLots.Select(x => x.LotId));
+            dl.EXCLUDE_LOTS = string.Join(";", log.ExcludedLots.Select(x => x.LotId));
+            string dispatchInfo = string.Join(";", log.PassedLots.Select(x => $"{x.GetLot().LotId};{x.GetLot().ArrivalTime:yyyy-MM-dd HH:mm:ss}"));
+            dl.DISPATCH_INFO = dispatchInfo;
+
+            OutputMart.Instance.AddData(OutputTable.DISPATCH_LOG, dl);
+        }
+    }
+}
---
+++ FabScheduler/PhotoEquipmentModel.cs
@@ -0,0 +1,46 @@
+﻿using DataMart.Input;
+using DataMart.SqlMapper;
+using FabSchedulerModel.InputEntity;
+using SimulationEngine.BaseEntity;
+using SimulationEngine.Common;
+using SimulationEngine.SimulationEntity;
+using SimulationEngine.SimulationInterface;
+using SimulationEngine.SimulationObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabSchedulerModel
+{
+    public class PhotoEquipmentModel : ISimEquipmentModel
+    {
+        public IEnumerable<Equipment> GetEqpList()
+        {
+            List<PhotoEquipment> returnList = new List<PhotoEquipment>();
+            List<EQUIPMENT> eqpList = InputMart.Instance.GetList<EQUIPMENT>(InputTable.EQUIPMENT);
+            foreach (EQUIPMENT eqp in eqpList)
+            {
+                PhotoEquipment pe = new PhotoEquipment();
+                pe.EqpId = eqp.EQP_ID;
+                Enum.TryParse<EqpState>(eqp.EQP_STATE, true, out EqpState eqpState);
+                pe.State = eqpState;
+
+                returnList.Add(pe); 
+            }
+            return returnList;
+        }
+
+        public IEnumerable<string> GetLoadableEqpIds(SimLot lot)
+        {
+            List<EQP_ARRANGE> arrangeList = InputMart.Instance.GetList<EQP_ARRANGE>(InputTable.EQP_ARRANGE);
+            string stepId = lot.GetLot().StepId;
+            string prodId = lot.GetLot().ProductId;
+            string procId = lot.GetLot().ProcessId;
+            List<string> photoEqpIds = arrangeList.FindAll(x => x.Equals(stepId) && x.Equals(prodId) && x.Equals(procId)).Select(x=>x.EQP_ID).ToList();
+
+            return photoEqpIds; 
+        }
+    }
+}
