83b635038e2f0a028448e0892cb5ba1897c3f75f
ChangRyeol <changryeol.recruit@gmail.com>
2025-02-28 10:17:51 +0900

    [Model] Photo 공정 input entity 정의

---
+++ FabScheduler/InputEntity/PhotoLot.cs
@@ -0,0 +1,25 @@
+﻿using SimulationEngine.BaseEntity;
+using SimulationEngine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabSchedulerModel.InputEntity
+{
+    public class PhotoLot : Lot
+    {
+        public Step initStep { get; set; }
+        public PhotoLot(int lotQty = 10)
+        {
+            State = LotState.WAIT;
+            LotQty = lotQty;
+            initStep = null; 
+        }
+        public PhotoLot(Step initStep, Equipment equipment, LotState state)
+        {
+            this.initStep = initStep;
+        }
+    }
+}
---
+++ FabScheduler/InputEntity/PhotoStep.cs
@@ -0,0 +1,14 @@
+﻿using SimulationEngine.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabSchedulerModel.InputEntity
+{
+    public class PhotoStep : Step 
+    {
+        public string StepName { get; set; }
+    }
+}
