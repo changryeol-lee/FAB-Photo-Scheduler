ac9c88cb27b02b09dc9d141e80b9e6a6c02327d4
ChangRyeol <changryeol.recruit@gmail.com>
2025-03-07 20:50:22 +0900

    [WEB UI] 공통 - type 단언 제거

--- WebUI/FrontEnd/simulation-web-ui/src/api/axiosInstance.ts
+++ WebUI/FrontEnd/simulation-web-ui/src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // 환경 변수에서 API Gateway URL과 API Key 가져오기
-const apiUrl = process.env.API_GATEWAY_URL as string
-const apiKey = process.env.API_KEY as string
+const apiUrl = process.env.API_GATEWAY_URL
+const apiKey = process.env.API_KEY
 
 const api = axios.create({
   baseURL: apiUrl, // API Gateway 기본 URL 설정
