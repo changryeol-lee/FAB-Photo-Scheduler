d3eb6b23ef8e71545f018feca163c99076c39f29
ChangRyeol <changryeol.recruit@gmail.com>
2025-03-18 21:48:03 +0900

    [WEB UI] Search Panel 컴포넌트 추가
    - 검색 조건 통일화를 위해, 상단 Search 부분을 컴포넌트로 생성함
    - search 관련 css분리함

---
+++ WebUI/FrontEnd/simulation-web-ui/src/components/SearchPanel.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="control-container">
+    <div class="control-wrapper q-px-md">
+      <div class="control-menu-name">{{ title }}</div>
+      <div class="header-container q-pa-md">
+        <div class="filter-row">
+          <!-- slot으로 전달 -->
+          <slot name="filter-items"></slot>
+          <div class="search-button-container">
+            <q-btn
+              :color="buttonColor"
+              :icon-right="buttonIcon"
+              :label="buttonLabel"
+              no-caps
+              class="search-button"
+              @click="$emit('search')"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  // 패널 제목
+  title: {
+    type: String,
+    default: '검색 조건',
+  },
+  // 검색 버튼 텍스트
+  buttonLabel: {
+    type: String,
+    default: '검색',
+  },
+  // 검색 버튼 아이콘
+  buttonIcon: {
+    type: String,
+    default: 'search',
+  },
+  // 검색 버튼 색상
+  buttonColor: {
+    type: String,
+    default: 'primary',
+  },
+})
+
+// 이벤트 정의
+defineEmits(['search'])
+</script>
